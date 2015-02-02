609a7243518d9e8a54695a04fdd4b9acda1bcb8c
akartynnik <alex.kar.008@gmail.com>
2015-02-03 01:36:36 +0300

    completed issues #6 for mantisbt v1.3.X

--- plugins/Templater/custom-features/name_as_link_fix_for_v1.3.js
+++ plugins/Templater/custom-features/name_as_link_fix_for_v1.3.js
@@ -1,9 +1,23 @@
 $(function () {
-	$("#buglist tbody tr:not(.buglist-footer)").each(function(){										
-		var linkHref = $(this).find("td.column-id a").attr("href");
-		var bugName = $(this).find("td.column-summary").text();
-		if(typeof linkHref != 'undefined'){
-			$(this).find("td.column-summary").html("<a href=\"" + linkHref + "\" class=\"not-link-hover-underline\">" + bugName + "</a>");
-		}
-	});
+	
+	if( window.location.pathname == "/view_all_bug_page.php"){
+		$("#buglist tbody tr:not(.buglist-footer)").each(function(){										
+			var linkHref = $(this).find("td.column-id a").attr("href");
+			var bugName = $(this).find("td.column-summary").text();
+			if(typeof linkHref != 'undefined'){
+				$(this).find("td.column-summary").html("<a href=\"" + linkHref + "\" class=\"not-link-hover-underline\">" + bugName + "</a>");
+			}
+		});
+	}
+	
+	if( window.location.pathname == "/my_view_page.php" ){
+		$(".myview_boxes_area tr.my-buglist-bug").each(function(){										
+			var linkHref = $(this).find("td.my-buglist-id span.small a:not(.edit)").attr("href");
+			var bugName = $(this).find("td.my-buglist-description").html();
+			if(typeof linkHref != 'undefined'){
+				$(this).find("td.my-buglist-description").html("<a href=\"" + linkHref + "\" class=\"not-link-hover-underline\">" + bugName + "</a>");
+			}
+		});
+	}
+	
 });
